c9af64b02f5ee7167936f06824ad6ad7a5b5d060
atelyshev <atelyshev@vosvideo.com>
2018-10-24 21:02:19 -0400

    Add get roles

--- chicago-ext/src/main/java/com/chicago/ext/dal/cassandra/PermissionDalImpl.java
+++ chicago-ext/src/main/java/com/chicago/ext/dal/cassandra/PermissionDalImpl.java
@@ -11,15 +11,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-import static com.chicago.ext.dal.cassandra.CassandraConstants.KEYSPACE;
-import static com.chicago.ext.dal.cassandra.CassandraConstants.PERMISSIONS_TABLE;
-import static com.chicago.ext.dal.cassandra.CassandraConstants.USER_PERMISSIONS_TABLE;
+import static com.chicago.ext.dal.cassandra.CassandraConstants.*;
 
 public class PermissionDalImpl implements PermissionDal
 {
@@ -46,8 +43,8 @@ public class PermissionDalImpl implements PermissionDal
                 .where(QueryBuilder.eq("user_id", UUID.fromString(userId)));
 
         ResultSet result = _cassandraConnector.getSession().execute(query);
-        Row row = result.one();
         UserOuterClass.UserPermissions.Builder builder = UserOuterClass.UserPermissions.newBuilder();
+        Row row = result.one();
 
         if (row != null)
         {
@@ -80,6 +77,44 @@ public class PermissionDalImpl implements PermissionDal
     @Override
     public PermissionOuterClass.Roles getSystemPermissions()
     {
-        return null;
+        Statement query = QueryBuilder.select().from(KEYSPACE, ROLES_TABLE);
+        ResultSet rolesResult = _cassandraConnector.getSession().execute(query);
+        List<PermissionOuterClass.Role> rolesArr = new ArrayList<>();
+        Row roleRow;
+
+        while ((roleRow = rolesResult.one()) != null)
+        {
+            UUID roleId = roleRow.getUUID("role_id");
+            String roleName = roleRow.getString("role_name");
+            String roleDescription = roleRow.getString("description");
+            Set<UUID> permissionIds = roleRow.getSet("permission_ids", UUID.class);
+            query = QueryBuilder.select().from(KEYSPACE, PERMISSIONS_TABLE)
+                    .where(QueryBuilder.in("permission_id", permissionIds));
+            ResultSet permissionsResult = _cassandraConnector.getSession().execute(query);
+
+            Row permissionRow;
+            List<PermissionOuterClass.Permission> permissionsArr = new ArrayList<>();
+            while ((permissionRow = permissionsResult.one()) != null)
+            {
+                PermissionOuterClass.Permission permission = PermissionOuterClass.Permission.newBuilder()
+                        .setPermissionId(permissionRow.getInt("permission_id"))
+                        .setPermissionName(permissionRow.getString("permission_name"))
+                        .setPermissionDescription(permissionRow.getString("description"))
+                        .build();
+                permissionsArr.add(permission);
+            }
+            PermissionOuterClass.Role role = PermissionOuterClass.Role.newBuilder()
+                    .setRoleId(roleId.toString())
+                    .setRoleName(roleName)
+                    .setRoleDescription(roleDescription)
+                    .addAllPermissions(permissionsArr)
+                    .build();
+            rolesArr.add(role);
+        }
+        PermissionOuterClass.Roles roles = PermissionOuterClass.Roles.newBuilder()
+                .addAllRole(rolesArr)
+                .build();
+
+        return roles;
     }
 }
