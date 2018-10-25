8c35026fda6e4676ef89a919795db00e758fd327
atelyshev@vosvideo.com <atelyshev@vosvideo.com>
2018-10-25 00:22:33 -0400

    Implemented permissions server pipeline

--- chicago-rest/src/main/java/com/chicago/services/controllers/PermissionController.java
+++ chicago-rest/src/main/java/com/chicago/services/controllers/PermissionController.java
@@ -17,7 +17,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-@Path("/position")
+@Path("/permissions")
 public class PermissionController
 {
     @Inject
