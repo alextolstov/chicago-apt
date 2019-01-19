e4916a30836a903f2e93b29ea1e1a2f57a94c675
atelyshev@vosvideo.com <atelyshev@vosvideo.com>
2019-01-18 20:10:30 -0500

    Tables

--- chicago-ext/src/main/java/com/chicago/ext/dal/InventoryDal.java
+++ chicago-ext/src/main/java/com/chicago/ext/dal/InventoryDal.java
@@ -1,5 +1,6 @@
 package com.chicago.ext.dal;
 
+import com.chicago.dto.Inventory;
 import com.chicago.dto.OrganizationOuterClass;
 
 public interface InventoryDal
@@ -7,4 +8,16 @@ public interface InventoryDal
     String createInventory(String organizationId) throws Exception;
 
     void updateInventory(OrganizationOuterClass.Organization organization);
+
+    Inventory.InventoryItem createInventoryItem(Inventory.InventoryItem inventoryItem);
+
+    void updateInventoryItem(Inventory.InventoryItem inventoryItem);
+
+    void applyInventoryOperation(Inventory.InventoryOperation inventoryOperation);
+
+    void startInventoryTransfer(Inventory.InventoryTransfer inventoryTransfer);
+
+    void acceptInventoryTransfer(Inventory.InventoryTransfer inventoryTransfer);
+
+    void rejectInventoryTransfer(Inventory.InventoryTransfer inventoryTransfer);
 }
