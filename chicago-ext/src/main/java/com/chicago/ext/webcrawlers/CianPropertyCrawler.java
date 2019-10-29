1f959d30fc02a23764adb14faf444096ce23df9a
suntolstov <suntolstov@gmail.com>
2019-10-28 23:21:34 -0400

    Merge remote-tracking branch 'remotes/origin/master' into feature/test
    # Conflicts:
    #	chicago-app/src/test/java/com/chicago/app/test/TestSearchFilters.java
    #	chicago-ext/src/main/java/com/chicago/ext/dal/mysql/CianSearchFiltersDalImpl.java

--- chicago-ext/src/main/java/com/chicago/ext/webcrawlers/CianPropertyCrawler.java
+++ chicago-ext/src/main/java/com/chicago/ext/webcrawlers/CianPropertyCrawler.java
@@ -164,7 +164,59 @@ public class CianPropertyCrawler implements PropertyCrawler<CianSearchFiltersMod
             //org.jsoup.Connection  con = org.jsoup.helper.HttpConnection.connect(httpRequest);
             Element body = doc.body();
             Elements divsPrice = body.getElementsByClass("c6e8ba5398--header--1dF9r");
+            Elements divsPricePerSquare = body.getElementsByClass("c6e8ba5398--term--3kvtJ");
+            // c6e8ba5398--single_title--22TGT - "1-комн. кв., 36 м², 25/25 этаж"
 
+            //  c6e8ba5398--title--39crr - "Светлая и просторная 1ккв 40,7м2"
+            // c6e8ba5398--subtitle--UTwbQ - "1-комн. кв., 41 м², 15/21 этаж"
+
+            // c6e8ba5398--single_title--22TGT - "1-комн. кв., 37 м², 8/12 этаж"
+            // c6e8ba5398--single_title--22TGT - "2-комн. кв., 45 м², 2/9 этаж"
+
+            // c6e8ba5398--title--39crr - "Лучшая цена в Приморском районе "
+            // c6e8ba5398--subtitle--UTwbQ" - "1-комн. кв., 37 м², 5/19 этаж"
+
+            Elements total = body.getElementsByClass("_93444fe79c--totalOffers--22-FL");
+            // paging (page 2) class="_93444fe79c--list-item--2KxXr">
+            // https://spb.cian.ru/cat.php?currency=2&deal_type=sale&engine_version=2&maxprice=5000000&object_type%5B0%5D=1&offer_type=flat
+            // &p=2&region=2&room1=1&room2=1&room3=1
+
+            // real elements
+            // class="_93444fe79c--wrapper--E9jWb">
+            // repeated   class="_93444fe79c--card--_yguQ">
+
+            // top container
+            // class="c6e8ba5398--offer-container--2sOFx c6e8ba5398--top3--1QmHz" data-reactroot=""
+            // class="c6e8ba5398--container--Y5gG9"
+            // class="c6e8ba5398--main-container--1FMpY"
+
+            // just elements
+            // class="c6e8ba5398--main--332Qx"
+            // class="c6e8ba5398--info-container--A11gU"
+            // class="c6e8ba5398--info--1fcZi"
+            // class="undefined c6e8ba5398--main-info--oWcMk"
+
+
+
+
+
+
+
+/*
+
+
+            private String propertyId;
+            private byte[] floorPlan;
+            private EnumTypes.PropertyType typeId;
+
+            private EnumTypes.ViewFromWindow windowsView;
+            private Boolean balcony;
+            private float kitchenSize;
+            private float ceilingHeight;
+            private int floor;
+            private int floorsInHouse;
+            private int roomsNumber;
+            */
         }
         catch (Exception ex)
         {
